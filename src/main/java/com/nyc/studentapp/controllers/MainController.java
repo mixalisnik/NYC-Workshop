830b06d75f782d09b948abd1d0b35dae6df57bb1
Haris Chatzianastasiou <harischatzi@tutanota.com>
2025-01-11 20:22:57 +0200

    Added frontend design

--- src/main/java/com/nyc/studentapp/controllers/MainController.java
+++ src/main/java/com/nyc/studentapp/controllers/MainController.java
@@ -1,10 +1,19 @@
 package com.nyc.studentapp.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/")
+    public String index() {
+        logger.info("Accessing index page");
+        return "index";
+    }
 
     @GetMapping("/helloWorld")
     public String helloWorld() {
