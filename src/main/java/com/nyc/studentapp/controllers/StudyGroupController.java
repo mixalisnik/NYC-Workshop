80d4c5a417cc38c691246e50d872604541a07f49
Haris Chatzianastasiou <harischatzi@tutanota.com>
2025-01-13 11:15:50 +0200

    Revert "Update"
    This reverts commit d811fd3f895dcc3b14e9abaa0161e064ef4adb04.

--- src/main/java/com/nyc/studentapp/controllers/StudyGroupController.java
+++ src/main/java/com/nyc/studentapp/controllers/StudyGroupController.java
@@ -1,105 +1,115 @@
 package com.nyc.studentapp.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
-// TODO: Add imports 
+import com.nyc.studentapp.entities.StudyGroup;
+import com.nyc.studentapp.services.StudyGroupService;
 
 @Controller
 public class StudyGroupController {
-// TODO: Uncomment this when you have the service
 
-    // private final StudyGroupService studyGroupService;
+    private final StudyGroupService studyGroupService;
 
-    // @Autowired
-    // public StudyGroupController(StudyGroupService studyGroupService) {
-    //     this.studyGroupService = studyGroupService;
-    // }
+    @Autowired
+    public StudyGroupController(StudyGroupService studyGroupService) {
+        this.studyGroupService = studyGroupService;
+    }
 
-    // @GetMapping("/studygroup")
-    // public String index() {
-    //     return "studygroupIndex";
-    // }
+    @GetMapping("/studygroup")
+    public String index() {
+        return "studygroupIndex";
+    }
 
-    // // Test endpoints for development
-    // @GetMapping("/studygroup/test/create")
-    // @ResponseBody
-    // public ResponseEntity<StudyGroup> testCreateGroup() {
-    //     StudyGroup newGroup = new StudyGroup();
-    //     newGroup.setGroupName("Test Study Group");
-    //     newGroup.setGroupDescription("This is a test study group for development");
-    //     StudyGroup createdGroup = studyGroupService.createStudyGroup(newGroup);
-    //     return ResponseEntity.ok(createdGroup);
-    // }
+    // Test endpoints for development
+    @GetMapping("/studygroup/test/create")
+    @ResponseBody
+    public ResponseEntity<StudyGroup> testCreateGroup() {
+        StudyGroup newGroup = new StudyGroup();
+        newGroup.setGroupName("Test Study Group");
+        newGroup.setGroupDescription("This is a test study group for development");
+        StudyGroup createdGroup = studyGroupService.createStudyGroup(newGroup);
+        return ResponseEntity.ok(createdGroup);
+    }
 
-    // @GetMapping("/studygroup/test/update/{id}")
-    // @ResponseBody
-    // public ResponseEntity<StudyGroup> testUpdateGroup(@PathVariable Integer id) {
-    //     try {
-    //         StudyGroup group = studyGroupService.getStudyGroupById(id).orElse(null);
-    //         if (group == null) {
-    //             return ResponseEntity.notFound().build();
-    //         }
-    //         group.setGroupName("Updated Test Group");
-    //         StudyGroup updatedGroup = studyGroupService.updateStudyGroup(group);
-    //         return ResponseEntity.ok(updatedGroup);
-    //     } catch (IllegalArgumentException e) {
-    //         return ResponseEntity.notFound().build();
-    //     }
-    // }
+    @GetMapping("/studygroup/test/update/{id}")
+    @ResponseBody
+    public ResponseEntity<StudyGroup> testUpdateGroup(@PathVariable Integer id) {
+        try {
+            StudyGroup group = studyGroupService.getStudyGroupById(id).orElse(null);
+            if (group == null) {
+                return ResponseEntity.notFound().build();
+            }
+            group.setGroupName("Updated Test Group");
+            StudyGroup updatedGroup = studyGroupService.updateStudyGroup(group);
+            return ResponseEntity.ok(updatedGroup);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-    // @GetMapping("/studygroup/test/delete/{id}")
-    // @ResponseBody
-    // public ResponseEntity<Void> testDeleteGroup(@PathVariable Integer id) {
-    //     try {
-    //         studyGroupService.deleteStudyGroup(id);
-    //         return ResponseEntity.ok().build();
-    //     } catch (IllegalArgumentException e) {
-    //         return ResponseEntity.notFound().build();
-    //     }
-    // }
+    @GetMapping("/studygroup/test/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> testDeleteGroup(@PathVariable Integer id) {
+        try {
+            studyGroupService.deleteStudyGroup(id);
+            return ResponseEntity.ok().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-    // // Actual CRUD endpoints for the frontend
-    // @PostMapping("/studygroup/create")
-    // @ResponseBody
-    // public ResponseEntity<StudyGroup> createStudyGroup(@RequestBody StudyGroup studyGroup) {
-    //     StudyGroup createdGroup = studyGroupService.createStudyGroup(studyGroup);
-    //     return ResponseEntity.ok(createdGroup);
-    // }
+    // Actual CRUD endpoints for the frontend
+    @PostMapping("/studygroup/create")
+    @ResponseBody
+    public ResponseEntity<StudyGroup> createStudyGroup(@RequestBody StudyGroup studyGroup) {
+        StudyGroup createdGroup = studyGroupService.createStudyGroup(studyGroup);
+        return ResponseEntity.ok(createdGroup);
+    }
 
-    // @GetMapping("/studygroup/read")
-    // @ResponseBody
-    // public ResponseEntity<List<StudyGroup>> getAllStudyGroups() {
-    //     List<StudyGroup> groups = studyGroupService.getAllStudyGroups();
-    //     return ResponseEntity.ok(groups);
-    // }
+    @GetMapping("/studygroup/read")
+    @ResponseBody
+    public ResponseEntity<List<StudyGroup>> getAllStudyGroups() {
+        List<StudyGroup> groups = studyGroupService.getAllStudyGroups();
+        return ResponseEntity.ok(groups);
+    }
 
-    // @GetMapping("/studygroup/read/{id}")
-    // @ResponseBody
-    // public ResponseEntity<StudyGroup> getStudyGroupById(@PathVariable Integer id) {
-    //     return ResponseEntity.ok(studyGroupService.getStudyGroupById(id).orElseThrow());
-    // }
+    @GetMapping("/studygroup/read/{id}")
+    @ResponseBody
+    public ResponseEntity<StudyGroup> getStudyGroupById(@PathVariable Integer id) {
+        return ResponseEntity.ok(studyGroupService.getStudyGroupById(id).orElseThrow());
+    }
 
-    // @PutMapping("/studygroup/update/{id}")
-    // @ResponseBody
-    // public ResponseEntity<StudyGroup> updateStudyGroup(@PathVariable Integer id, @RequestBody StudyGroup studyGroup) {
-    //     try {
-    //         studyGroup.setId(id);
-    //         StudyGroup updatedGroup = studyGroupService.updateStudyGroup(studyGroup);
-    //         return ResponseEntity.ok(updatedGroup);
-    //     } catch (IllegalArgumentException e) {
-    //         return ResponseEntity.notFound().build();
-    //     }
-    // }
+    @PutMapping("/studygroup/update/{id}")
+    @ResponseBody
+    public ResponseEntity<StudyGroup> updateStudyGroup(@PathVariable Integer id, @RequestBody StudyGroup studyGroup) {
+        try {
+            studyGroup.setId(id);
+            StudyGroup updatedGroup = studyGroupService.updateStudyGroup(studyGroup);
+            return ResponseEntity.ok(updatedGroup);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-    // @DeleteMapping("/studygroup/delete/{id}")
-    // @ResponseBody
-    // public ResponseEntity<Void> deleteStudyGroup(@PathVariable Integer id) {
-    //     try {
-    //         studyGroupService.deleteStudyGroup(id);
-    //         return ResponseEntity.ok().build();
-    //     } catch (IllegalArgumentException e) {
-    //         return ResponseEntity.notFound().build();
-    //     }
-    // }
+    @DeleteMapping("/studygroup/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> deleteStudyGroup(@PathVariable Integer id) {
+        try {
+            studyGroupService.deleteStudyGroup(id);
+            return ResponseEntity.ok().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 } 
